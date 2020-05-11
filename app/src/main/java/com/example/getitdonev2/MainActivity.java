7214574bc5be53e4af41822bcf5d6dd510c764ee
constantin_timis <timis.constantin99@gmail.com>
2020-05-11 23:40:51 +0300

    All profile functionalities

--- app/src/main/java/com/example/getitdonev2/MainActivity.java
+++ app/src/main/java/com/example/getitdonev2/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.getitdonev2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.app.AlertDialog;
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
@@ -20,9 +26,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(bottomNavigationView, navController);
-        NavigationUI.setupActionBarWithNavController(this, navController);
+        //remove the button login item
+        //bottomNavigationView.getMenu().removeItem(R.id.loginFragment);
+
+         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+         NavigationUI.setupActionBarWithNavController(this, navController);
+
+
 
     }
+
+
+
 }
